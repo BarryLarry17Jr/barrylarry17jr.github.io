4839f9a60bfcfd965957b44ca92543c5b146a297
BarryLarry17Jr <44936363+BarryLarry17Jr@users.noreply.github.com>
2020-02-20 16:39:44 +1100

    Darts Beta Update
    Nigger Beans

---
+++ Darts/darts.js
@@ -0,0 +1,52 @@
+function plusonClick501() {
+    clicks += 501;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function plusonClick301() {
+    clicks += 301;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function plusonClick1() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function minusonClick1() {
+    clicks -= 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function plusonClick5() {
+    clicks += 5;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function minusonClick5() {
+    clicks -= 5;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function plusonClick10() {
+    clicks += 10;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function minusonClick10() {
+    clicks -= 10;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function plusonClick20() {
+    clicks += 20;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+function minusonClick20() {
+    clicks -= 20;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+
+///////////////////////////
+
